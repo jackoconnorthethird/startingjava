1163f2c1ca4cc97b49347e8c25b7e49b5cd1f143
Jack O'Coder <48263185+jackoconnorthethird@users.noreply.github.com>
2021-01-11 12:24:14 -0600

    Add files via upload

---
+++ startingjava.java
@@ -0,0 +1,7 @@
+public class startingjava {
+    public static void main(String[] args){
+        System.out.println("My name is Coding Dojo");
+        System.out.println("i am 100 years old");
+        System.out.println("i live in Chicago IL");
+    }
+}
